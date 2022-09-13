2336262b38589da8ca56b6c7c8ebe74eee461043
Ishan Garg <ishan.garg10394@gmail.com>
2022-09-13 20:36:25 +0530

    Merged With Auth

--- manage.py
+++ manage.py
@@ -2,9 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
+
 
 
 def main():
+    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cexchange.settings')
     try:
